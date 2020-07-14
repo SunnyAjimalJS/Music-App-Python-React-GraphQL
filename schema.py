cfdaef606e225d13614024728a0ad7b15e4a0137
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-07-14 17:48:14 +0100

    Used Mutation to dynamically add new Users with default values for uid and createdAt

--- schema.py
+++ schema.py
@@ -1,12 +1,13 @@
 import graphene
 import json
+import uuid
 from datetime import datetime
 
 
 class User(graphene.ObjectType):
-    id = graphene.ID()
+    id = graphene.ID(default_value=str(uuid.uuid4()))
     username = graphene.String()
-    created_at = graphene.DateTime()
+    created_at = graphene.DateTime(default_value=datetime.now())
 
 
 class Query(graphene.ObjectType):
@@ -27,15 +28,32 @@ class Query(graphene.ObjectType):
         ][:limit]
 
 
-schema = graphene.Schema(query=Query)
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(User)
+
+    class Arguments:
+        username = graphene.String()
+
+    def mutate(self, info, username):
+        user = User(username=username)
+        return CreateUser(user=user)
+
+
+class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
 
 result = schema.execute(
     '''
-    {
-        users(limit:1) {
-            id
-            username
-            createdAt
+    mutation {
+        createUser(username: "Jeff"){
+            user{
+                id
+                username
+                createdAt
+            }
         }
     }
     '''
