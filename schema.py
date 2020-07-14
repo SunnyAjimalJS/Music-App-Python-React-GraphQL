6fbdb285fc40222b27f53edbea081d824a0564c0
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-07-14 16:27:51 +0100

    Added a User class with subclasses to add Object Types and Arguments in Queries. Also added an optional limit in the Query for pagination

--- schema.py
+++ schema.py
@@ -1,12 +1,30 @@
 import graphene
 import json
+from datetime import datetime
+
+
+class User(graphene.ObjectType):
+    id = graphene.ID()
+    username = graphene.String()
+    created_at = graphene.DateTime()
 
 
 class Query(graphene.ObjectType):
+    users = graphene.List(User, limit=graphene.Int())
     hello = graphene.String()
+    is_admin = graphene.Boolean()
 
     def resolve_hello(self, info):
         return "world"
+
+    def resolve_is_admin(self, info):
+        return True
+
+    def resolve_users(self, info, limit=None):
+        return [
+            User(id="1", username="Fred", created_at=datetime.now()),
+            User(id="2", username="Doug", created_at=datetime.now())
+        ][:limit]
 
 
 schema = graphene.Schema(query=Query)
@@ -14,10 +32,14 @@ schema = graphene.Schema(query=Query)
 result = schema.execute(
     '''
     {
-        hello
+        users(limit:1) {
+            id
+            username
+            createdAt
+        }
     }
     '''
 )
 
 dictResult = dict(result.data.items())
-print(json.dumps(dictResult))
+print(json.dumps(dictResult, indent=2))
