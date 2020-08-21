4658f7ca27403b5295283c9647c2a2e6a0ce4087
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-08-21 17:17:19 +0100

    Set up Apollo Client to execute Queries with React Apollo

--- react-frontend-client/src/components/Auth/Register.js
+++ react-frontend-client/src/components/Auth/Register.js
@@ -20,7 +20,7 @@ const Register = ({ classes }) => {
   return <div>Register</div>;
 };
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
     width: "auto",
     display: "block",
@@ -29,37 +29,37 @@ const styles = theme => ({
     [theme.breakpoints.up("md")]: {
       width: 400,
       marginLeft: "auto",
-      marginRight: "auto"
-    }
+      marginRight: "auto",
+    },
   },
   paper: {
     marginTop: theme.spacing.unit * 8,
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    padding: theme.spacing.unit * 2
+    padding: theme.spacing.unit * 2,
   },
   title: {
     marginTop: theme.spacing.unit * 2,
-    color: theme.palette.openTitle
+    color: theme.palette.openTitle,
   },
   avatar: {
     margin: theme.spacing.unit,
-    backgroundColor: theme.palette.secondary.main
+    backgroundColor: theme.palette.secondary.main,
   },
   form: {
     width: "100%",
-    marginTop: theme.spacing.unit
+    marginTop: theme.spacing.unit,
   },
   submit: {
     marginTop: theme.spacing.unit * 2,
-    marginBottom: theme.spacing.unit * 2
+    marginBottom: theme.spacing.unit * 2,
   },
   icon: {
     padding: "0px 2px 2px 0px",
     verticalAlign: "middle",
-    color: "green"
-  }
+    color: "green",
+  },
 });
 
 export default withStyles(styles)(Register);
