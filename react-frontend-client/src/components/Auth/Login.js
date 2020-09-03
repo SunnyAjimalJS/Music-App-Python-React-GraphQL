33b99472264d8ad09fc20dcfac508bdbe057074c
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-09-03 13:59:51 +0100

    Started working on a Login page

--- react-frontend-client/src/components/Auth/Login.js
+++ react-frontend-client/src/components/Auth/Login.js
@@ -1,19 +1,99 @@
 import React from "react";
+import { Mutation } from "react-apollo";
+import { gql } from "apollo-boost";
+
 import withStyles from "@material-ui/core/styles/withStyles";
-// import Typography from "@material-ui/core/Typography";
-// import Avatar from "@material-ui/core/Avatar";
-// import FormControl from "@material-ui/core/FormControl";
-// import Paper from "@material-ui/core/Paper";
-// import Input from "@material-ui/core/Input";
-// import InputLabel from "@material-ui/core/InputLabel";
-// import Button from "@material-ui/core/Button";
-// import Lock from "@material-ui/icons/Lock";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import FormControl from "@material-ui/core/FormControl";
+import Paper from "@material-ui/core/Paper";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import Button from "@material-ui/core/Button";
+import Lock from "@material-ui/icons/Lock";
 
 const Login = ({ classes, setNewUser }) => {
-  return <div>Login</div>;
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <Gavel />
+        </Avatar>
+        <Typography variant="headline">Register</Typography>
+
+        <Mutation
+          mutation={REGISTER_MUTATION}
+          variables={{ username, email, password }}
+          onCompleted={(data) => {
+            console.log({ data });
+            setOpen(true);
+          }}
+        >
+          {(createUser, { loading, error }) => {
+            return (
+              <form
+                onSubmit={(event) => handleSubmit(event, createUser)}
+                className={classes.form}
+              >
+                <FormControl margin="normal" required fullWidth>
+                  <InputLabel htmlFor="username">Username</InputLabel>
+                  <Input
+                    id="username"
+                    onChange={(event) => setUsername(event.target.value)}
+                  />
+                </FormControl>
+                <FormControl margin="normal" required fullWidth>
+                  <InputLabel htmlFor="email">Email</InputLabel>
+                  <Input
+                    id="email"
+                    type="email"
+                    onChange={(event) => setEmail(event.target.value)}
+                  />
+                </FormControl>
+                <FormControl margin="normal" required fullWidth>
+                  <InputLabel htmlFor="password">Password</InputLabel>
+                  <Input
+                    id="password"
+                    type="password"
+                    onChange={(event) => setPassword(event.target.value)}
+                  />
+                </FormControl>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="secondary"
+                  disabled={
+                    loading ||
+                    !username.trim() ||
+                    !email.trim() ||
+                    !password.trim()
+                  }
+                  className={classes.submit}
+                >
+                  {loading ? "Registering..." : "Register"}
+                </Button>
+                <Button
+                  onClick={() => setNewUser(false)}
+                  color="primary"
+                  variant="outlined"
+                  fullWidth
+                >
+                  Previous user? Log in here
+                </Button>
+
+                {/* Error Handling */}
+                {error && <Error error={error} />}
+              </form>
+            );
+          }}
+        </Mutation>
+      </Paper>
+    </div>
+  );
 };
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
     width: "auto",
     display: "block",
@@ -22,32 +102,32 @@ const styles = theme => ({
     [theme.breakpoints.up("md")]: {
       width: 400,
       marginLeft: "auto",
-      marginRight: "auto"
-    }
+      marginRight: "auto",
+    },
   },
   paper: {
     marginTop: theme.spacing.unit * 8,
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    padding: theme.spacing.unit * 2
+    padding: theme.spacing.unit * 2,
   },
   title: {
     marginTop: theme.spacing.unit * 2,
-    color: theme.palette.secondary.main
+    color: theme.palette.secondary.main,
   },
   avatar: {
     margin: theme.spacing.unit,
-    backgroundColor: theme.palette.primary.main
+    backgroundColor: theme.palette.primary.main,
   },
   form: {
     width: "100%",
-    marginTop: theme.spacing.unit
+    marginTop: theme.spacing.unit,
   },
   submit: {
     marginTop: theme.spacing.unit * 2,
-    marginBottom: theme.spacing.unit * 2
-  }
+    marginBottom: theme.spacing.unit * 2,
+  },
 });
 
 export default withStyles(styles)(Login);
