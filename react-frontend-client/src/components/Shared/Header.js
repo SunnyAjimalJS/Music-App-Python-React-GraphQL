ee70e43175d764015a0686af2d17ca9a660ea11e
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-09-04 15:49:51 +0100

    Started building out the Header component with Material UI

--- react-frontend-client/src/components/Shared/Header.js
+++ react-frontend-client/src/components/Shared/Header.js
@@ -1,40 +1,44 @@
 import React from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import RadioIcon from "@material-ui/icons/RadioTwoTone";
-// import FaceIcon from "@material-ui/icons/FaceTwoTone";
-// import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import RadioIcon from "@material-ui/icons/RadioTwoTone";
+import FaceIcon from "@material-ui/icons/FaceTwoTone";
+import Typography from "@material-ui/core/Typography";
 
 const Header = ({ classes }) => {
-  return <div>Header</div>;
+  return (
+    <AppBar position="static" className={classes.root}>
+      <Toolbar>{/* Title / Logo */}</Toolbar>
+    </AppBar>
+  );
 };
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
     flexGrow: 1,
     margin: 0,
-    padding: 0
+    padding: 0,
   },
   grow: {
     flexGrow: 1,
     display: "flex",
     alignItems: "center",
-    textDecoration: "none"
+    textDecoration: "none",
   },
   logo: {
     marginRight: theme.spacing.unit,
-    fontSize: 45
+    fontSize: 45,
   },
   faceIcon: {
     marginRight: theme.spacing.unit,
     fontSize: 30,
-    color: "white"
+    color: "white",
   },
   username: {
     color: "white",
-    fontSize: 30
-  }
+    fontSize: 30,
+  },
 });
 
 export default withStyles(styles)(Header);
