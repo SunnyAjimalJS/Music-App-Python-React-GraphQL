808aad18a7586808c4ce9af285b7f35e03b95fb5
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-09-09 15:44:05 +0100

    Started building out the App page and imported Search, TrackList and Create Track components - all rendering fine

--- react-frontend-client/src/pages/App.js
+++ react-frontend-client/src/pages/App.js
@@ -1,16 +1,28 @@
 import React from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
 
+import SearchTracks from "../components/Track/SearchTracks";
+import TrackList from "../components/Track/TrackList";
+import CreateTrack from "../components/Track/CreateTrack";
+
 const App = ({ classes }) => {
-  return <div>App</div>;
+  return (
+    <>
+      <div className={classes.container}>
+        <SearchTracks />
+        <CreateTrack />
+        <TrackList />
+      </div>
+    </>
+  );
 };
 
-const styles = theme => ({
+const styles = (theme) => ({
   container: {
     margin: "0 auto",
     maxWidth: 960,
-    padding: theme.spacing.unit * 2
-  }
+    padding: theme.spacing.unit * 2,
+  },
 });
 
 export default withStyles(styles)(App);
