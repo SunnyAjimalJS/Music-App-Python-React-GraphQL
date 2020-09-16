01c72400327882a697e70a9d37e33a6c7cd1996a
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-09-16 12:21:20 +0100

    Added if statement to compare the selected file's size and filesize limit

--- react-frontend-client/src/components/Track/CreateTrack.js
+++ react-frontend-client/src/components/Track/CreateTrack.js
@@ -31,7 +31,8 @@ const CreateTrack = ({ classes }) => {
   const handleAudioChange = (event) => {
     const selectedFile = event.target.files[0];
     const fileSizeLimit = 10000000; //10 million bytes - 10mb
-    setFile(selectedFile);
+    if (selectedFile && fileSizeLimit > selectedFile.size)
+      setFile(selectedFile);
   };
 
   const handleAudioUpload = async () => {
