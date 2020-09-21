6309808750175c41dd551658d6c3ce528385323f
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-09-21 12:54:56 +0100

    Started working on functionality for Updating Tracks

--- react-frontend-client/src/components/Track/UpdateTrack.js
+++ react-frontend-client/src/components/Track/UpdateTrack.js
@@ -1,50 +1,219 @@
-import React from "react";
-import withStyles from "@material-ui/core/styles/withStyles";
-// import IconButton from "@material-ui/core/IconButton";
-// import EditIcon from "@material-ui/icons/Edit";
-// import Button from "@material-ui/core/Button";
-// import TextField from "@material-ui/core/TextField";
-// import Dialog from "@material-ui/core/Dialog";
-// import DialogActions from "@material-ui/core/DialogActions";
-// import DialogContent from "@material-ui/core/DialogContent";
-// import DialogContentText from "@material-ui/core/DialogContentText";
-// import FormControl from "@material-ui/core/FormControl";
-// import FormHelperText from "@material-ui/core/FormHelperText";
-// import DialogTitle from "@material-ui/core/DialogTitle";
-// import CircularProgress from "@material-ui/core/CircularProgress";
-// import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
+import React, { useState } from "react";
+import { Mutation } from "react-apollo";
+import { gql } from "apollo-boost";
+import axios from "axios";
 
-const UpdateTrack = ({ classes }) => {
-  return <div>UpdateTrack</div>;
+import withStyles from "@material-ui/core/styles/withStyles";
+import IconButton from "@material-ui/core/IconButton";
+import EditIcon from "@material-ui/icons/Edit";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import FormControl from "@material-ui/core/FormControl";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
+
+import { GET_TRACKS_QUERY } from "../../pages/App";
+import Error from "../Shared/Error";
+
+const UpdateTrack = ({ classes, track }) => {
+  const [open, setOpen] = useState(false);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [file, setFile] = useState("");
+  const [submitting, setSubmitting] = useState(false);
+  const [fileError, setFileError] = useState("");
+
+  const handleAudioChange = (event) => {
+    const selectedFile = event.target.files[0];
+    const fileSizeLimit = 10000000; //10 million bytes - 10mb
+    if (selectedFile && selectedFile.size > fileSizeLimit) {
+      setFileError(`${selectedFile.name}: File size is too large!`);
+    } else {
+      setFile(selectedFile);
+      setFileError("");
+    }
+  };
+
+  const handleAudioUpload = async () => {
+    try {
+      // Metadata for Cloudinary request:
+      const data = new FormData();
+      data.append("file", file);
+      data.append("resource_type", "raw");
+      data.append("upload_preset", "react-python-tracks");
+      data.append("cloud_name", "digensvyy");
+      // Request:
+      const res = await axios.post(
+        "https://api.cloudinary.com/v1_1/digensvyy/raw/upload",
+        data
+      );
+      return res.data.url;
+    } catch (err) {
+      console.error("Error uploading file!", err);
+      setSubmitting(false);
+    }
+  };
+
+  const handleSubmit = async (event, createTrack) => {
+    event.preventDefault();
+    setSubmitting(true);
+    // upload the audio file and get returned url from API
+    const uploadedUrl = await handleAudioUpload();
+    createTrack({ variables: { title, description, url: uploadedUrl } });
+  };
+
+  return (
+    <>
+      {/* Create Track Button */}
+      <Button
+        onClick={() => setOpen(true)}
+        variant="fab"
+        className={classes.fab}
+        color="secondary"
+      >
+        {open ? <ClearIcon /> : <AddIcon />}
+      </Button>
+
+      {/* Create Track Dialog */}
+      <Mutation
+        mutation={CREATE_TRACK_MUTATION}
+        onCompleted={(data) => {
+          setSubmitting(false);
+          setOpen(false);
+          setTitle("");
+          setDescription("");
+          setFile("");
+        }}
+        refetchQueries={() => [{ query: GET_TRACKS_QUERY }]}
+      >
+        {(createTrack, { loading, error }) => {
+          if (error) return <Error error={error} />;
+
+          return (
+            <Dialog open={open} className={classes.dialog}>
+              <form onSubmit={(event) => handleSubmit(event, createTrack)}>
+                <DialogTitle>Create Track</DialogTitle>
+                <DialogContent>
+                  <DialogContentText>
+                    Add a Title, Description & Audio File (Must be under 10MB)
+                  </DialogContentText>
+                  <FormControl fullWidth>
+                    <TextField
+                      label="Title"
+                      placeholder="Add Title"
+                      onChange={(event) => setTitle(event.target.value)}
+                      value={title}
+                      className={classes.textField}
+                    />
+                  </FormControl>
+                  <FormControl fullWidth>
+                    <TextField
+                      multiline
+                      rows="2"
+                      label="Description"
+                      onChange={(event) => setDescription(event.target.value)}
+                      value={description}
+                      placeholder="Add Description"
+                      className={classes.textField}
+                    />
+                  </FormControl>
+
+                  {/* Input handling  */}
+                  <FormControl error={Boolean(fileError)}>
+                    <input
+                      id="audio"
+                      required={true}
+                      type="file"
+                      accept="audio/mp3,audio/wav"
+                      className={classes.input}
+                      onChange={handleAudioChange}
+                    />
+
+                    {/* Add Audio File Button */}
+                    <label htmlFor="audio">
+                      <Button
+                        variant="outlined"
+                        color={file ? "secondary" : "inherit"}
+                        component="span"
+                        className={classes.button}
+                      >
+                        Audio File
+                        <LibraryMusicIcon className={classes.icon} />
+                      </Button>
+
+                      {/* Text to display the uploaded file name */}
+                      {file && file.name}
+                    </label>
+                    <FormHelperText>{fileError}</FormHelperText>
+                  </FormControl>
+                </DialogContent>
+                <DialogActions>
+                  <Button
+                    disabled={submitting}
+                    onClick={() => setOpen(false)}
+                    className={classes.cancel}
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    disabled={
+                      submitting ||
+                      !title.trim() ||
+                      !description.trim() ||
+                      !file
+                    }
+                    type="submit"
+                    className={classes.save}
+                  >
+                    {submitting ? (
+                      <CircularProgress className={classes.save} size={24} />
+                    ) : (
+                      "Add Track"
+                    )}
+                  </Button>
+                </DialogActions>
+              </form>
+            </Dialog>
+          );
+        }}
+      </Mutation>
+    </>
+  );
 };
 
-const styles = theme => ({
+const styles = (theme) => ({
   container: {
     display: "flex",
-    flexWrap: "wrap"
+    flexWrap: "wrap",
   },
   dialog: {
     margin: "0 auto",
-    maxWidth: 550
+    maxWidth: 550,
   },
   textField: {
-    margin: theme.spacing.unit
+    margin: theme.spacing.unit,
   },
   cancel: {
-    color: "red"
+    color: "red",
   },
   save: {
-    color: "green"
+    color: "green",
   },
   button: {
-    margin: theme.spacing.unit * 2
+    margin: theme.spacing.unit * 2,
   },
   icon: {
-    marginLeft: theme.spacing.unit
+    marginLeft: theme.spacing.unit,
   },
   input: {
-    display: "none"
-  }
+    display: "none",
+  },
 });
 
 export default withStyles(styles)(UpdateTrack);
