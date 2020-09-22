e13d46f473d6576c6e67bd20fa992aca66a065ef
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-09-22 11:44:08 +0100

    Started working on functionality for deleting a track

--- react-frontend-client/src/components/Track/DeleteTrack.js
+++ react-frontend-client/src/components/Track/DeleteTrack.js
@@ -1,6 +1,8 @@
-import React from "react";
-// import IconButton from "@material-ui/core/IconButton";
-// import TrashIcon from "@material-ui/icons/DeleteForeverOutlined";
+import React, { useContext } from "react";
+import IconButton from "@material-ui/core/IconButton";
+import TrashIcon from "@material-ui/icons/DeleteForeverOutlined";
+
+import { UserContext } from "../../Root";
 
 const DeleteTrack = () => {
   return <div>DeleteTrack</div>;
