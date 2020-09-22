c010767055bc95d95b7f90a84b23b63bd18e4b35
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-09-22 15:21:06 +0100

    Added the Mutation to the return for LikeTrack and passed in the CREATE_LIKE_MUTATION as a mutation prop

--- react-frontend-client/src/components/Track/LikeTrack.js
+++ react-frontend-client/src/components/Track/LikeTrack.js
@@ -7,7 +7,7 @@ import IconButton from "@material-ui/core/IconButton";
 import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 
 const LikeTrack = ({ classes }) => {
-  return <div>LikeTrack</div>;
+  return <Mutation mutation={CREATE_LIKE_MUTATION}></Mutation>;
 };
 
 const CREATE_LIKE_MUTATION = gql`
