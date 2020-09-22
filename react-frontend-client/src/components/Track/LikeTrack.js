3625fcfedfc6c3ed119a0343dccf830dbfdfb947
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-09-22 15:18:12 +0100

    Wrote out gql query for Create Like Mutation

--- react-frontend-client/src/components/Track/LikeTrack.js
+++ react-frontend-client/src/components/Track/LikeTrack.js
@@ -1,19 +1,35 @@
 import React from "react";
+import { Mutation } from "react-apollo";
+import { gql } from "apollo-boost";
+
 import withStyles from "@material-ui/core/styles/withStyles";
-// import IconButton from "@material-ui/core/IconButton";
-// import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import IconButton from "@material-ui/core/IconButton";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 
 const LikeTrack = ({ classes }) => {
   return <div>LikeTrack</div>;
 };
 
-const styles = theme => ({
+const CREATE_LIKE_MUTATION = gql`
+  mutation($trackId: Int!) {
+    createLike(trackId: $trackId) {
+      track {
+        id
+        likes {
+          id
+        }
+      }
+    }
+  }
+`;
+
+const styles = (theme) => ({
   iconButton: {
-    color: "deeppink"
+    color: "deeppink",
   },
   icon: {
-    marginLeft: theme.spacing.unit / 2
-  }
+    marginLeft: theme.spacing.unit / 2,
+  },
 });
 
 export default withStyles(styles)(LikeTrack);
