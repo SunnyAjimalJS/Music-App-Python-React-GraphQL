6e4160b50d179c9eef62813704f783ba1070be1d
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-09-25 09:17:59 +0100

    Imported format from date functions to format the date displayed for when a user joined

--- react-frontend-client/src/pages/Profile.js
+++ react-frontend-client/src/pages/Profile.js
@@ -10,6 +10,7 @@ import Typography from "@material-ui/core/Typography";
 import ThumbUpIcon from "@material-ui/icons/ThumbUpTwoTone";
 import AudiotrackIcon from "@material-ui/icons/AudiotrackTwoTone";
 import Divider from "@material-ui/core/Divider";
+import format from "date-fns/format";
 
 import Loading from "../components/Shared/Loading";
 import Error from "../components/Shared/Error";
