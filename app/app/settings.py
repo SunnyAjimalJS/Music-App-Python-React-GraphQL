29065b8e3157bcc6f558555e2a8b79bdec57d480
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2020-09-25 10:53:50 +0100

    Added heroku

--- app/app/settings.py
+++ app/app/settings.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -25,7 +26,7 @@ SECRET_KEY = 't0dw2=s_4lqg7(6x$=a0fvzpts@+pn14qi&$haou5b!cs2)r7x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['evening-journey-15372.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
